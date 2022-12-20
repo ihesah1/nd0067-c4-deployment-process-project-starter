7922fdd7666ce5d5cd0f2195fd5bd3fa2f331b9d
EC2 Default User <ec2-user@ip-172-31-86-126.ec2.internal>
2022-12-20 12:26:56 +0000

    first commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=hessahdb
+export POSTGRES_HOST=database-2.cq1jzjip3mq6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::hessahlocal
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=https://675ef5d80b6e4079acb1d69f890b34d3.vfs.cloud9.us-east-1.amazonaws.com
